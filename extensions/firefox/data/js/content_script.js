e5e250f6c0b5c0b4814e21f170db5bcdf159bf0a
Hiroyuki Sano <sh19910711@gmail.com>
2015-08-12 19:18:36 +0900

    wip: content_script

--- extensions/firefox/data/js/content_script.js
+++ extensions/firefox/data/js/content_script.js
@@ -1,3 +1,9 @@
 var hello = document.createElement("div");
-hello.innerHTML = "hello";
+hello.innerHTML = "hello<br>";
 document.body.appendChild(hello);
+
+self.port.on("hello", function() {
+  hello.innerHTML += "add-on said hello<br>";
+});
+
+self.port.emit("hello");

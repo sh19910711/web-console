fd50e8d53f10d2aa9c986da78e54808c83bd3ee3
Hiroyuki Sano <sh19910711@gmail.com>
2016-06-09 18:33:46 +0900

    wip: response

--- lib/web_console/response.rb
+++ lib/web_console/response.rb
@@ -1,23 +1,42 @@
 module WebConsole
   # A response object that writes content before the closing </body> tag, if
   # possible.
-  #
-  # The object quacks like Rack::Response.
-  class Response < Struct.new(:body, :status, :headers)
-    def write(content)
-      raw_body = Array(body).first.to_s
+  class Response < Rack::Response
+    def insert_head(content)
+      insert_after '<head>', content
+    end
 
-      if position = raw_body.rindex('</body>')
-        raw_body.insert(position, content)
-      else
-        raw_body << content
+    def insert_body(content)
+      insert_before '</body>', content
+    end
+
+    private
+
+      def insert_before(*args)
+        insert(*args) do |body, tag|
+          body.index(tag)
+        end
       end
 
-      self.body = raw_body
-    end
+      def insert_after(*args)
+        insert(*args) do |body, tag|
+          if pos = body.rindex(tag)
+            pos + tag.length
+          end
+        end
+      end
 
-    def finish
-      Rack::Response.new(body, status, headers).finish
-    end
+      def insert(tag, content)
+        raw_body = Array(body).first.to_s
+
+        if position = yield(raw_body, tag)
+          raw_body.insert(position, content)
+        else
+          raw_body << content
+        end
+
+        self.body = raw_body
+        initialize raw_body, status, headers
+      end
   end
 end

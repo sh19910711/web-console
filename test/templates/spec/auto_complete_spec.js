05fbacd84a150d5d10675f504e4b1b076550d35b
Hiroyuki Sano <sh19910711@gmail.com>
2016-06-22 07:15:40 +0000

    wip: test/templates

--- test/templates/spec/auto_complete_spec.js
+++ test/templates/spec/auto_complete_spec.js
@@ -3,13 +3,13 @@ describe("Auto Complete", function() {
     context("set up with three elements", function() {
       beforeEach(function() {
         var self = this.autoComplete = new AutoComplete(["something", "somewhat", "somewhere"]);
-        this.assertSelected = function(pos) {
+        this.moveNext = function(times) {
+          for (var i = 0; i < times; ++i) self.next();
+        };
+        this.assertSelect = function(pos) {
+          assert.ok(hasClass(self.view.children[pos], 'selected'));
           for (var i = 0; i < self.view.children.length; ++i) {
-            if (i === pos) {
-              assert.ok(hasClass(self.view.children[i], 'selected'));
-            } else {
-              assert.ok(!hasClass(self.view.children[i], 'selected'));
-            }
+            if (i !== pos) assert.ok(!hasClass(self.view.children[i], 'selected'));
           }
         };
       });
@@ -21,40 +21,22 @@ describe("Auto Complete", function() {
       });
 
       context("move next two times", function() {
-        beforeEach(function() {
-          this.autoComplete.next();
-          this.autoComplete.next();
-        });
-        it("should point the 1-th element", function() {
-          this.assertSelected(1);
-        });
+        beforeEach(function() { this.moveNext(2) });
+        it("should point the 1-th element", function() { this.assertSelect(1); });
 
         context("back once", function() {
-          beforeEach(function() {
-            this.autoComplete.back();
-          });
-          it("should point the 0-th element", function() {
-            this.assertSelected(0);
-          });
+          beforeEach(function() { this.autoComplete.back(); });
+          it("should point the 0-th element", function() { this.assertSelect(0); });
 
           context("back once again", function() {
-            beforeEach(function() {
-              this.autoComplete.back();
-            });
-            it("should point the last element", function() {
-              this.assertSelected(2);
-            });
+            beforeEach(function() { this.autoComplete.back(); });
+            it("should point the last element", function() { this.assertSelect(2); });
           });
         });
 
         context("move next two times again", function() {
-          beforeEach(function() {
-            this.autoComplete.next();
-            this.autoComplete.next();
-          });
-          it("should back to the first of list", function() {
-            this.assertSelected(0);
-          });
+          beforeEach(function() { this.moveNext(2) });
+          it("should back to the first of list", function() { this.assertSelect(0); });
         });
       });
     });

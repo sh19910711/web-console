bae9c3bf990a26cd81aed0d39290aafcf617d878
Hiroyuki Sano <sh19910711@gmail.com>
2016-07-08 09:25:47 +0000

    wip

--- test/templates/test/auto_complete_test.js
+++ test/templates/test/auto_complete_test.js
@@ -1,18 +1,14 @@
-suite('AutocompleteTests', function() {
+suite('Autocomplete', function() {
   setup(function() {
     var self = this;
     self.ac = new Autocomplete(['other', 'other2', 'something', 'somewhat', 'somewhere', 'test']);
-    self.assertSelect = function(pos) {
-      assert.equal(self.ac.current, pos);
-    };
-    self.assertCount = function(className, cnt) {
-      assert.equal(self.ac.view.getElementsByClassName(className).length, cnt);
-    }
+    self.assertSelect = function(pos) { assert.equal(self.ac.current, pos); };
+    self.assertCount = function(className, cnt) { assert.equal(self.ac.view.getElementsByClassName(className).length, cnt); }
     self.inc = function(prefix) { self.ac.refine(prefix, true); };
     self.dec = function(prefix) { self.ac.refine(prefix, false); };
   });
 
-  test('inc()', function() {
+  test('empty string', function() {
     this.inc('');
 
     assert(!this.ac.confirmed);
@@ -21,7 +17,16 @@ suite('AutocompleteTests', function() {
     this.assertCount('hidden', 0)
   });
 
-  test('inc(some)', function() {
+  test('undefined', function() {
+    this.dec();
+
+    assert(!this.ac.confirmed);
+    assert.equal(this.ac.left, 0);
+    assert.equal(this.ac.right, this.ac.words.length);
+    this.assertCount('hidden', 0);
+  });
+
+  test('refining', function() {
     this.inc('some');
 
     assert(!this.ac.confirmed);
@@ -30,7 +35,7 @@ suite('AutocompleteTests', function() {
     this.assertCount('hidden', 3);
   });
 
-  test('inc(somet)', function() {
+  test('confirmed', function() {
     this.inc('somet');
 
     assert.equal(this.ac.confirmed, 'something');
@@ -38,16 +43,7 @@ suite('AutocompleteTests', function() {
     assert.equal(this.ac.right, 3);
   });
 
-  test('inc(somewh)', function() {
-    this.inc('somewh');
-
-    this.assertCount('hidden', 4);
-    assert(!this.ac.confirmed);
-    assert.equal(this.ac.left, 3);
-    assert.equal(this.ac.right, this.ac.words.length - 1);
-  });
-
-  test('inc(o) => dec()', function() {
+  test('decrement', function() {
     this.inc('o');
     this.dec('');
 
@@ -56,7 +52,7 @@ suite('AutocompleteTests', function() {
     this.assertCount('hidden', 0);
   });
 
-  test('inc(other) => dec() => inc(some)', function() {
+  test('inc => dec => inc', function() {
     this.inc('other');
     this.dec('');
     this.inc('some');

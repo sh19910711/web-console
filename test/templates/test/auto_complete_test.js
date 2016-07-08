604121b22806912231b6cd790f5fe96af852e7eb
Hiroyuki Sano <sh19910711@gmail.com>
2016-07-08 08:22:27 +0000

    wip

--- test/templates/test/auto_complete_test.js
+++ test/templates/test/auto_complete_test.js
@@ -1,7 +1,7 @@
 suite('AutocompleteTests', function() {
   setup(function() {
     var self = this;
-    self.ac = new Autocomplete(['something', 'somewhat', 'somewhere']);
+    self.ac = new Autocomplete(['other', 'other2', 'something', 'somewhat', 'somewhere', 'test']);
     self.assertSelect = function(pos) {
       assert.equal(self.ac.current, pos);
     };
@@ -12,31 +12,55 @@ suite('AutocompleteTests', function() {
 
   test('inc()', function() {
     this.ac.inc('');
+
     assert(!this.ac.confirmed);
+    assert.equal(this.ac.left, 0);
+    assert.equal(this.ac.right, this.ac.words.length);
     this.assertCount('hidden', 0)
   });
 
   test('inc(some)', function() {
     this.ac.inc('some');
+
     assert(!this.ac.confirmed);
-    this.assertCount('hidden', 0);
+    assert.equal(this.ac.left, 2);
+    assert.equal(this.ac.right, this.ac.words.length - 1);
+    this.assertCount('hidden', 3);
   });
 
   test('inc(somet)', function() {
     this.ac.inc('somet');
+
     assert.equal(this.ac.confirmed, 'something');
+    assert.equal(this.ac.left, 2);
+    assert.equal(this.ac.right, 3);
   });
 
   test('inc(somewh)', function() {
     this.ac.inc('somewh');
+
+    this.assertCount('hidden', 4);
     assert(!this.ac.confirmed);
-    this.assertCount('hidden', 1);
+    assert.equal(this.ac.left, 3);
+    assert.equal(this.ac.right, this.ac.words.length - 1);
   });
 
-  test('inc(somewh) => dec(some)', function() {
-    this.ac.inc('somewh');
-    this.ac.dec('some');
-    assert(!this.ac.confirmed);
+  test('inc(o) => dec()', function() {
+    this.ac.inc('o');
+    this.ac.dec('');
+
+    assert.equal(this.ac.left, 0);
+    assert.equal(this.ac.right, this.ac.words.length);
     this.assertCount('hidden', 0);
+  });
+
+  test('inc(other) => dec() => inc(some)', function() {
+    this.ac.inc('other');
+    this.ac.dec('');
+    this.ac.inc('some');
+
+    assert.equal(this.ac.left, 2);
+    assert.equal(this.ac.right, this.ac.words.length - 1);
+    this.assertCount('hidden', 3);
   });
 });

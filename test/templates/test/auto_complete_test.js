eb1e1c46f367ac5e0da463e6a5a62335d8a471c8
Hiroyuki Sano <sh19910711@gmail.com>
2016-07-16 07:42:32 +0000

    wip: auto_complete_test

--- test/templates/test/auto_complete_test.js
+++ test/templates/test/auto_complete_test.js
@@ -2,8 +2,6 @@ suite('Autocomplete', function() {
   setup(function() {
     var self = this;
     self.ac = new Autocomplete(['other', 'other2', 'something', 'somewhat', 'somewhere', 'test']);
-    self.assertSelect = function(pos) { assert.equal(self.ac.current, pos); };
-    self.assertCount = function(className, cnt) { assert.equal(self.ac.view.getElementsByClassName(className).length, cnt); }
     self.inc = function(prefix) { self.ac.refine(prefix, true); };
     self.dec = function(prefix) { self.ac.refine(prefix, false); };
   });
@@ -12,53 +10,57 @@ suite('Autocomplete', function() {
     this.inc('');
 
     assert(!this.ac.confirmed);
-    assert.equal(this.ac.left, 0);
-    assert.equal(this.ac.right, this.ac.words.length);
-    this.assertCount('hidden', 0)
+    assertRange(this, 0, this.ac.words.length, 0);
   });
 
   test('undefined', function() {
     this.dec();
 
     assert(!this.ac.confirmed);
-    assert.equal(this.ac.left, 0);
-    assert.equal(this.ac.right, this.ac.words.length);
-    this.assertCount('hidden', 0);
+    assertRange(this, 0, this.ac.words.length, 0);
   });
 
   test('refining', function() {
     this.inc('some');
 
     assert(!this.ac.confirmed);
-    assert.equal(this.ac.left, 2);
-    assert.equal(this.ac.right, this.ac.words.length - 1);
-    this.assertCount('hidden', 3);
+    assertRange(this, 2, this.ac.words.length - 1, 3);
   });
 
   test('confirmed', function() {
     this.inc('somet');
 
     assert.equal(this.ac.confirmed, 'something');
-    assert.equal(this.ac.left, 2);
-    assert.equal(this.ac.right, 3);
+    assertRange(this, 2, 3);
   });
 
   test('decrement', function() {
     this.inc('o');
     this.dec('');
 
-    assert.equal(this.ac.left, 0);
-    assert.equal(this.ac.right, this.ac.words.length);
-    this.assertCount('hidden', 0);
+    assertRange(this, 0, this.ac.words.length, 0);
   });
 
-  test('inc => dec => inc', function() {
+  test('other => empty => some', function() {
     this.inc('other');
     this.dec('');
     this.inc('some');
 
-    assert.equal(this.ac.left, 2);
-    assert.equal(this.ac.right, this.ac.words.length - 1);
-    this.assertCount('hidden', 3);
+    assertRange(this, 2, this.ac.words.length - 1, 3);
   });
+
+  test('some => empty', function() {
+    this.inc('some');
+    this.dec('');
+
+    assertRange(this, 0, this.ac.words.length - 1);
+  });
+
+  // private helpers
+
+  function assertRange(self, left, right, hiddenCnt) {
+    assert.equal(left, self.ac.left, 'left');
+    assert.equal(right, self.ac.right, 'right');
+    if (hiddenCnt) assert.equal(hiddenCnt, self.ac.view.getElementsByClassName('hidden').length, 'hiddenCnt');
+  }
 });

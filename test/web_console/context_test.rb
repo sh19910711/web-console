1ff9ed74d94ebbd591e3fae9a6d3f50433829088
Hiroyuki Sano <sh19910711@gmail.com>
2016-07-16 06:52:13 +0000

    wip: context_test

--- test/web_console/context_test.rb
+++ test/web_console/context_test.rb
@@ -2,27 +2,27 @@ require 'test_helper'
 
 module WebConsole
   class ContextTest < ActiveSupport::TestCase
-    test '#of(empty) includes local variables' do
+    test '#extract(empty) includes local variables' do
       local_var = 'local'
-      assert Context.new(binding).of('').include?(:local_var)
+      assert Context.new(binding).extract('').include?(:local_var)
     end
 
-    test '#of(empty) includes instance variables' do
+    test '#extract(empty) includes instance variables' do
       @instance_var = 'instance'
-      assert Context.new(binding).of('').include?(:@instance_var)
+      assert Context.new(binding).extract('').include?(:@instance_var)
     end
 
-    test '#of(empty) includes global variables' do
+    test '#extract(empty) includes global variables' do
       $global_var = 'global'
-      assert Context.new(binding).of('').include?(:$global_var)
+      assert Context.new(binding).extract('').include?(:$global_var)
     end
 
-    test '#of(obj) returns methods' do
-      assert Context.new(binding).of('Rails').include?('Rails.root')
+    test '#extract(obj) returns methods' do
+      assert Context.new(binding).extract('Rails').include?('Rails.root')
     end
 
-    test '#of(obj) returns constants' do
-      assert Context.new(binding).of('WebConsole').include?('WebConsole::Middleware')
+    test '#extract(obj) returns constants' do
+      assert Context.new(binding).extract('WebConsole').include?('WebConsole::Middleware')
     end
   end
 end

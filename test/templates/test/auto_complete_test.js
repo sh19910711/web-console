9780cd8ea297aa0e27c3cae476c4531c937d9778
Hiroyuki Sano <sh19910711@gmail.com>
2016-07-25 01:16:55 +0000

    wip

--- test/templates/test/auto_complete_test.js
+++ test/templates/test/auto_complete_test.js
@@ -4,6 +4,42 @@ suite('Autocomplete', function() {
     this.refine = function(prefix) { this.ac.refine(prefix); };
   });
 
+  test('shows only five elements around the current element', function() {
+    var ac = new Autocomplete(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']);
+
+    // A B C D E ...
+    assertNotClass(ac, 0, 5, 'soft-hidden');
+    assertClass(ac, 5, ac.words.length, 'soft-hidden');
+
+    // A B C D E ...
+    ac.onKeyDown(TestHelper.keyDown(TestHelper.KEY_TAB));
+    assertNotClass(ac, 0, 5, 'soft-hidden');
+    assertClass(ac, 5, ac.words.length, 'soft-hidden');
+
+    // B C D E F ...
+    ac.onKeyDown(TestHelper.keyDown(TestHelper.KEY_TAB));
+    assertClass(ac, 0, 1, 'soft-hidden');
+    assertNotClass(ac, 1, 6, 'soft-hidden');
+    assertClass(ac, 6, ac.words.length, 'soft-hidden');
+
+    // D E F G H
+    for (var i = 0; i < 5; ++i) ac.onKeyDown(TestHelper.keyDown(TestHelper.KEY_TAB));
+    assert.equal(6, ac.current);
+    assertClass(ac, 0, 3, 'soft-hidden');
+    assertNotClass(ac, 3, ac.words.length, 'soft-hidden');
+
+    // D E F G H
+    ac.onKeyDown(TestHelper.keyDown(TestHelper.KEY_TAB));
+    assert.equal(7, ac.current);
+    assertClass(ac, 0, 3, 'soft-hidden');
+    assertNotClass(ac, 3, ac.words.length, 'soft-hidden');
+
+    // A B C D E ...
+    ac.onKeyDown(TestHelper.keyDown(TestHelper.KEY_TAB));
+    assertNotClass(ac, 0, 5, 'soft-hidden');
+    assertClass(ac, 5, ac.words.length, 'soft-hidden');
+  });
+
   test('does nothing if the word list is empty', function() {
     var ac = new Autocomplete([]);
     assert.doesNotThrow(function() {
@@ -54,6 +90,18 @@ suite('Autocomplete', function() {
 
     assertRange(this, 0, this.ac.words.length);
   });
+
+  function assertClass(ac, left, right, className) {
+    for (var i = left; i < right; ++i) {
+      assert.ok(hasClass(ac.view.children[i], className), i + '-th element shuold have ' + className);
+    }
+  }
+
+  function assertNotClass(ac, left, right, className) {
+    for (var i = left; i < right; ++i) {
+      assert.notOk(hasClass(ac.view.children[i], className), i + '-th element should not have ' + className);
+    }
+  }
 
   function assertRange(self, left, right, hiddenCnt) {
     assert.equal(left, self.ac.left, 'left');

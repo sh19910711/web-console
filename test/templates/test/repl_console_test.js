b4dac2ccd3800cb1622d3eb7740fc87577fb5d44
Hiroyuki Sano <sh19910711@gmail.com>
2016-08-14 19:45:06 +0900

    Add auto-suggestion and hinting area

--- test/templates/test/repl_console_test.js
+++ test/templates/test/repl_console_test.js
@@ -153,4 +153,30 @@ suite('REPLCosnole', function() {
       }, 100);
     });
   });
+
+  suite('Suggest Mode', function() {
+    test('shows first matched word in current context', function(done) {
+      var c = this.console;
+      c.setInput('some');
+      setTimeout(function() {
+        assert.match(c.promptDisplay.innerText, /^something/);
+        done();
+      }, 100);
+    });
+
+    test('hides automatically in a few seconds', function(done) {
+      var c = this.console;
+      c.suggestWait = 200;
+      c.setInput('some');
+
+      assert.equal('some\u00A0', c.promptDisplay.innerText);
+      setTimeout(function() {
+        assert.match(c.promptDisplay.innerText, /something/);
+        setTimeout(function() {
+          assert.equal('some\u00A0', c.promptDisplay.innerText);
+          done();
+        }, 200);
+      }, 100);
+    });
+  });
 });
